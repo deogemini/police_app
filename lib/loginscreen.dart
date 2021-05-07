42eadf606d500ab43b140b0b0bffc42545fed50b
deogemini <geminideogratias8@gmail.com>
2021-05-07 22:15:18 +0300

    creating login screen

--- lib/loginscreen.dart
+++ lib/loginscreen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:passwordfield/passwordfield.dart';
 
 class LoginScreen extends StatefulWidget {
   @override
@@ -6,21 +7,60 @@ class LoginScreen extends StatefulWidget {
 }
 
 class _LoginScreenState extends State<LoginScreen> {
+  final _formKey = GlobalKey<FormState>();
+
   @override
   Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+
     return Scaffold(
-      body: Center(
-          child: Column(children: <Widget>[
-        SizedBox(height: 100),
-        Container(
-          height: 118,
-          width: 90,
-          decoration: BoxDecoration(
-              image: DecorationImage(
-                  image: AssetImage('assets/policeIcon.png'),
-                  fit: BoxFit.cover)),
-        ),
-      ])),
-    );
+        body: Container(
+      width: double.infinity,
+      height: size.height,
+      child: Stack(
+        alignment: Alignment.center,
+        children: <Widget>[
+          Positioned(
+            top: 75,
+            left: 135,
+            height: 118,
+            width: 90,
+            child: Image.asset(
+              'assets/policeIcon.png',
+              width: size.width * 0.4,
+            ),
+          ),
+          Container(
+              margin: EdgeInsets.symmetric(vertical: 10),
+              padding: EdgeInsets.symmetric(horizontal: 20, vertical: 5),
+              width: size.width * 0.8,
+              decoration: BoxDecoration(
+                  color: Color(0xffcff7fa),
+                  borderRadius: BorderRadius.circular(20)),
+              child: TextField(
+                  decoration: InputDecoration(
+                      icon: Icon(Icons.account_box),
+                      hintText: "User Id",
+                      border: InputBorder.none))),
+          // Positioned(
+          //     top: 410,
+          //     child: Container(
+          //       margin: EdgeInsets.symmetric(vertical: 10),
+          //       padding: EdgeInsets.symmetric(horizontal: 20, vertical: 5),
+          //       width: size.width * 0.8,
+          //       decoration: BoxDecoration(
+          //           color: Color(0xffcff7fa),
+          //           borderRadius: BorderRadius.circular(20)),
+          //       child: TextField(
+          //           obscureText: true,
+          //           decoration: InputDecoration(
+          //               icon: Icon(Icons.lock),
+          //               hintText: "Password",
+          //               suffixIcon: Icon(Icons.visibility),
+          //               border: InputBorder.none)),
+          //     )),
+        ],
+      ),
+    ));
   }
 }
