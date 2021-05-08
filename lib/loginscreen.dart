9b180d697fbc931d4b9ac445f5dc5e12f6ba5dfa
deogemini <geminideogratias8@gmail.com>
2021-05-08 12:17:51 +0300

    first version login page accomplished

--- lib/loginscreen.dart
+++ lib/loginscreen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:passwordfield/passwordfield.dart';
+import 'package:flutter_svg/svg.dart';
 
 class LoginScreen extends StatefulWidget {
   @override
@@ -17,37 +18,40 @@ class _LoginScreenState extends State<LoginScreen> {
         body: Container(
       width: double.infinity,
       height: size.height,
-      child: Stack(
-        alignment: Alignment.center,
+      padding: EdgeInsets.all(12),
+      child: ListView(
         children: <Widget>[
-          Positioned(
-            top: 75,
-            left: 135,
-            height: 118,
+          SizedBox(
+            height: 75,
+          ),
+          Image.asset(
+            "assets/policeIcon.png",
             width: 90,
-            child: Image.asset(
-              'assets/policeIcon.png',
-              width: size.width * 0.4,
+            height: 100,
+          ),
+          SizedBox(
+            height: 67,
+          ),
+          Container(
+            margin: EdgeInsets.symmetric(vertical: 2),
+            padding: EdgeInsets.symmetric(horizontal: 20),
+            width: size.width * 0.8,
+            decoration: BoxDecoration(
+                color: Color(0xffcff7fa),
+                borderRadius: BorderRadius.circular(20)),
+            child: TextField(
+              decoration: InputDecoration(
+                  icon: Icon(Icons.account_box),
+                  hintText: "User Id",
+                  border: InputBorder.none),
             ),
           ),
-          Container(
-              margin: EdgeInsets.symmetric(vertical: 10),
-              padding: EdgeInsets.symmetric(horizontal: 20, vertical: 5),
-              width: size.width * 0.8,
-              decoration: BoxDecoration(
-                  color: Color(0xffcff7fa),
-                  borderRadius: BorderRadius.circular(20)),
-              child: TextField(
-                  decoration: InputDecoration(
-                      icon: Icon(Icons.account_box),
-                      hintText: "User Id",
-                      border: InputBorder.none))),
           SizedBox(
-            height: 70,
+            height: 10,
           ),
           Container(
-            margin: EdgeInsets.symmetric(vertical: 10),
-            padding: EdgeInsets.symmetric(horizontal: 20, vertical: 5),
+            margin: EdgeInsets.symmetric(vertical: 5),
+            padding: EdgeInsets.symmetric(horizontal: 20),
             width: size.width * 0.8,
             decoration: BoxDecoration(
                 color: Color(0xffcff7fa),
@@ -60,6 +64,33 @@ class _LoginScreenState extends State<LoginScreen> {
                     suffixIcon: Icon(Icons.visibility),
                     border: InputBorder.none)),
           ),
+          SizedBox(
+            height: 90,
+          ),
+          TextButton(
+            child: Text("Submit",
+                style: TextStyle(
+                    fontSize: 17,
+                    color: Colors.white,
+                    fontWeight: FontWeight.w600)),
+            style: ButtonStyle(
+              padding: MaterialStateProperty.all<EdgeInsets>(
+                  EdgeInsets.fromLTRB(69, 0, 69, 0)),
+              backgroundColor:
+                  MaterialStateProperty.all<Color>(Color(0xff03002E)),
+              shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(14.0),
+                ),
+              ),
+            ),
+            onPressed: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => LoginScreen()),
+              );
+            },
+          )
         ],
       ),
     ));
