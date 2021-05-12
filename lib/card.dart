e78deae2530b5bdd05b10676efbf3e1a007558a3
deogemini <geminideogratias8@gmail.com>
2021-05-12 07:51:45 +0300

    styling card

--- lib/card.dart
+++ lib/card.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:police_app/viewcases.dart';
 
-
 class Cards extends StatelessWidget {
   const Cards({
     Key key,
@@ -11,27 +10,29 @@ class Cards extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       child: Card(
+          borderOnForeground: true,
+          shadowColor: Colors.blue,
           child: Column(mainAxisSize: MainAxisSize.min, children: <Widget>[
-        const ListTile(
-          title: Text(
-              '82189291                                        09/12/2020'),
-        ),
-        Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: <Widget>[
-            TextButton(
-              child: const Text(
-                  'fwnkfnfkjrnjbefbhhjbjbewbbbfqehbbqbeqbhhqefrbq'),
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => viewCase()),
-                );
-              },
+            const ListTile(
+              title: Text(
+                  '82189291                                        09/12/2020'),
             ),
-          ],
-        ),
-      ])),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[
+                TextButton(
+                  child: const Text(
+                      'fwnkfnfkjrnjbefbhhjbjbewbbbfqehbbqbeqbhhqefrbq'),
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => viewCase()),
+                    );
+                  },
+                ),
+              ],
+            ),
+          ])),
     );
   }
 }
