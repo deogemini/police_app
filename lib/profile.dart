7c12404764460181e457f41acb9669879bef73f9
deogemini <geminideogratias8@gmail.com>
2021-05-12 19:02:33 +0300

    please the profile styling is ready

--- lib/profile.dart
+++ lib/profile.dart
@@ -14,46 +14,127 @@ class _ProfileState extends State<Profile> {
       Opacity(
           opacity: 0.3,
           child: Container(
-            decoration: BoxDecoration(
-                image: DecorationImage(
-              image: AssetImage(
-                "assets/policeIcon.png",
-              ),
-            )),
-          )),
+              decoration: BoxDecoration(
+                  image: DecorationImage(
+            image: AssetImage(
+              "assets/policeIcon.png",
+            ),
+          )))),
       Container(
-        padding: EdgeInsets.fromLTRB(16, 20, 17, 0),
-        child: Column(children: <Widget>[
-          Container(
-            child: Text('Profile Details Coming Soon',
-                textAlign: TextAlign.center,
-                style: TextStyle(
-                  fontWeight: FontWeight.bold,
-                )),
-          ),
-          TextButton(
-            child: Text("Login Here",
-                style: TextStyle(fontSize: 17, color: Colors.white)),
-            style: ButtonStyle(
-              padding: MaterialStateProperty.all<EdgeInsets>(
-                  EdgeInsets.fromLTRB(69, 0, 69, 0)),
-              backgroundColor:
-                  MaterialStateProperty.all<Color>(Color(0xff03002E)),
-              shape: MaterialStateProperty.all<RoundedRectangleBorder>(
-                RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(12.0),
+          padding: EdgeInsets.fromLTRB(30, 100, 24, 100),
+          child: Column(children: <Widget>[
+            Row(
+              children: <Widget>[
+                Text(
+                  'Name',
+                  style: TextStyle(fontWeight: FontWeight.w700, fontSize: 22),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(left: 110.0),
+                  child: Text(
+                    'Juma Juma',
+                    style: TextStyle(fontWeight: FontWeight.w100, fontSize: 22),
+                  ),
+                ),
+              ],
+            ),
+            SizedBox(
+              height: 25,
+            ),
+            Row(
+              children: <Widget>[
+                Text(
+                  'Number',
+                  style: TextStyle(fontWeight: FontWeight.w700, fontSize: 22),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(left: 120.0),
+                  child: Text(
+                    'PS33223',
+                    style: TextStyle(fontWeight: FontWeight.w100, fontSize: 22),
+                  ),
+                ),
+              ],
+            ),
+            SizedBox(
+              height: 25,
+            ),
+            Row(
+              children: <Widget>[
+                Text(
+                  'Gender',
+                  style: TextStyle(fontWeight: FontWeight.w700, fontSize: 22),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(left: 160.0),
+                  child: Text(
+                    'Male',
+                    style: TextStyle(fontWeight: FontWeight.w100, fontSize: 22),
+                  ),
+                ),
+              ],
+            ),
+            SizedBox(
+              height: 25,
+            ),
+            Row(
+              children: <Widget>[
+                Text(
+                  'Station',
+                  style: TextStyle(fontWeight: FontWeight.w700, fontSize: 22),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(left: 130.0),
+                  child: Text(
+                    'Osterbay',
+                    style: TextStyle(fontWeight: FontWeight.w100, fontSize: 22),
+                  ),
+                ),
+              ],
+            ),
+            SizedBox(
+              height: 25,
+            ),
+            Row(
+              children: <Widget>[
+                Text(
+                  'Role',
+                  style: TextStyle(fontWeight: FontWeight.w700, fontSize: 22),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(left: 150.0),
+                  child: Text(
+                    'Mpelelezi',
+                    style: TextStyle(fontWeight: FontWeight.w100, fontSize: 22),
+                  ),
+                ),
+              ],
+            ),
+            SizedBox(
+              height: 60,
+            ),
+            TextButton(
+              child: Text("Login Here",
+                  style: TextStyle(fontSize: 17, color: Colors.white)),
+              style: ButtonStyle(
+                padding: MaterialStateProperty.all<EdgeInsets>(
+                    EdgeInsets.fromLTRB(69, 0, 69, 0)),
+                backgroundColor:
+                    MaterialStateProperty.all<Color>(Color(0xff03002E)),
+                shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                  RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(12.0),
+                  ),
                 ),
               ),
-            ),
-            onPressed: () {
-              Navigator.push(
-                context,
-                MaterialPageRoute(builder: (context) => LoginScreen()),
-              );
-            },
-          )
-        ]),
-      )
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => LoginScreen()),
+                );
+              },
+            )
+          ]))
     ]));
   }
 }
