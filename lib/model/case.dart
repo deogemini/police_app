8a65bef1637ab315d2857d4d5a3f87a541b1f910
deogemini <geminideogratias8@gmail.com>
2021-06-20 20:09:10 +0300

    add: profile model added

--- lib/model/case.dart
+++ lib/model/case.dart
@@ -1,11 +1,11 @@
-class caseDetails {
+class CaseDetails {
   String description;
   String casenumber;
   String immediateAction;
 
-  caseDetails({this.casenumber, this.description, this.immediateAction});
+  CaseDetails({this.casenumber, this.description, this.immediateAction});
 
-  caseDetails.fromJson(Map<String, dynamic> json)
+  CaseDetails.fromJson(Map<String, dynamic> json)
       : description = json['description'],
         casenumber = json['status'],
         immediateAction = json['immeadteAction'];
--- lib/model/user.dart
+++ lib/model/user.dart
@@ -9,7 +9,7 @@ class User {
 
   User.fromJson(Map<String, dynamic> json)
       : role = json['role'],
-        id = json['id'],
+        id = json['_id'],
         name = json['name'],
         email = json['email'],
         title = json['title'];
