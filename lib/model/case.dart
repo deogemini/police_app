03f536177b94e2633b6afd5239cb3aefce86d755
deogemini <geminideogratias8@gmail.com>
2021-06-26 23:54:02 +0300

    refractor: changes in cases model

--- lib/model/case.dart
+++ lib/model/case.dart
@@ -1,39 +1,49 @@
 class CaseDetails {
   String id;
-  String description;
   String name;
   String age;
   String phone;
   String gender;
   String occupation;
   String nationality;
+  String description;
   String address;
-  String casenumber;
   String immediateAction;
+  String policeOfficeTakingIntialAction;
+  String finalDisposal;
+  String createdAt;
+  String casenumber;
 
-  CaseDetails(
-      {
-        this.id,
-        this.address,
-      this.age,
-      this.gender,
-      this.nationality,
-      this.phone,
-      this.occupation,
-      this.casenumber,
-      this.description,
-      this.immediateAction,
-       this. name});
+  CaseDetails({
+    this.id,
+    this.name,
+    this.age,
+    this.phone,
+    this.gender,
+    this.occupation,
+    this.nationality,
+    this.description,
+    this.address,
+    this.immediateAction,
+    this.policeOfficeTakingIntialAction,
+    this.finalDisposal,
+    this.createdAt,
+    this.casenumber,
+  });
 
   CaseDetails.fromJson(Map<String, dynamic> json)
-      : id = json['id'] ?? "id", 
-      description = json['description'] ?? "description",
-        casenumber = json['caseNumber'] ?? "caseNumber",
+      : id = json['id'],
+        name = json['name'],
+        age = json['age'],
+        phone = json['phone'],
+        gender = json['gender'],
+        occupation = json['occupation'],
+        nationality = json['nationality'],
+        description = json['description'],
+        address = json['address'],
         immediateAction = json['immeadteAction'],
-        occupation = json['occupation'] ?? "occupation",
-        phone = json['phone'] ?? "phone",
-        gender = json['gender'] ?? "gender",
-        name = json['name'] ?? "name",
-        address = json['address'] ?? "address",
-        nationality = json['nationality'] ?? "nationality";
+        policeOfficeTakingIntialAction = json['policeOfficeTakingIntialAction'],
+        finalDisposal = json['finalDisposal'],
+        createdAt = json['createdAt'],
+        casenumber = json['caseNumber'];
 }
